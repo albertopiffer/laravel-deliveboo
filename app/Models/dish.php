a8a024f739d883bb1ad8de8597746e763caa1942
Ant0C <cafeo97@gmail.com>
2023-05-18 17:21:17 +0200

    Feat relation m-t-m between dish, order

--- app/Models/dish.php
+++ app/Models/dish.php
@@ -9,4 +9,9 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class dish extends Model
 {
     use HasFactory, SoftDeletes;
+
+    public function orders(){
+        return $this->belongsToMany(Order::class);
+    }
+
 }
