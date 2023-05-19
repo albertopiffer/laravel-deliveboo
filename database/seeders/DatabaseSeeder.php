b42cf6907c77a8722f29251750fc4a421ddea37f
albertopiffer <albertopiffer96@gmail.com>
2023-05-19 12:35:22 +0200

    FEAT dishseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,7 +25,7 @@ class DatabaseSeeder extends Seeder
 
             TypologySeeder::class,
             RestaurantSeeder::class,
-            // DishSeeder::class,
+            DishSeeder::class,
             // OrderSeeder::class,
         ]);
     }
