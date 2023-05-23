943ceb149e43bee74083905f6772d08404d6c683
Giorgio.Bocchio <giorgiobocchio2@gmail.com>
2023-05-23 18:19:59 +0200

    index

--- resources/views/dishes/index.blade.php
+++ resources/views/dishes/index.blade.php
@@ -2,9 +2,25 @@
 @section('content')
     <h1>Index</h1>
 
-    @forelse ($dishes as $dish)
-    <p>{{$dish->name}}</p>
-    @empty
-        <p> Nessun piatto trovato </P>
-    @endforelse
+    <div class="container">
+        <div class="row">
+            @forelse ($dishes as $dish)
+                <div class="col-4 mb-5">
+                    <div class=" bg-white border border-secondary rounded p-2">
+                        <div class="d-flex">
+                            <img class="me-3" src="https://picsum.photos/100/100" alt="">
+                            <div>
+                                <p>{{$dish->name}}</p>
+                                <p>{{$dish->description}}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            @empty
+                <p> Nessun piatto trovato </P>
+            @endforelse
+        </div>
+    </div>
+
 @endsection
