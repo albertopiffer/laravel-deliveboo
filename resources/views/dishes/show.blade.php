17da355c85b10b0dae43cab8daf807eda0ab81ec
alessandrob17 <a.baldipardi@gmail.com>
2023-05-24 12:05:31 +0200

    FEAT box_shadow

--- resources/views/dishes/show.blade.php
+++ resources/views/dishes/show.blade.php
@@ -3,8 +3,8 @@
 <h1 class="text-center">Show</h1>
 
 <h1 class="text-center">{{$dish->name}}</h1>
- <div class="card bg-dark text-white col-6 m-auto  ">
-    <img src="https://picsum.photos/150/50" class="card-img" alt="...">
+ <div class="card bg-dark text-white col-6 m-auto box-shadow">
+    <img src="https://picsum.photos/150/50" class="card-img box-shadow" alt="...">
     <div class="card-img-overlay">
 
 
@@ -12,3 +12,17 @@
 </div>
 
 @endsection
+
+<style>
+    
+.box-shadow {
+    transition: transform .2s;
+    box-shadow: 0px 0px 14px 5px rgba(0,0,0,0.44); 
+}
+
+.box-shadow:hover{
+    transform: scale(1.05);
+}
+
+
+</style>
