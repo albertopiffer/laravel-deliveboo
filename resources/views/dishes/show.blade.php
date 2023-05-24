27858b14ebccc1a451562e121dc327f3925e8191
alessandrob17 <a.baldipardi@gmail.com>
2023-05-24 11:55:38 +0200

    FEAT bootstrap show

--- resources/views/dishes/show.blade.php
+++ resources/views/dishes/show.blade.php
@@ -1,6 +1,14 @@
 @extends('layouts.app')
 @section('content')
-    <h1>Show</h1>
+<h1 class="text-center">Show</h1>
 
-    <h1>{{$dish->name}}</h1>
+<h1 class="text-center">{{$dish->name}}</h1>
+ <div class="card bg-dark text-white col-6 m-auto  ">
+    <img src="https://picsum.photos/150/50" class="card-img" alt="...">
+    <div class="card-img-overlay">
+
+
+    </div>
+</div>
+
 @endsection
