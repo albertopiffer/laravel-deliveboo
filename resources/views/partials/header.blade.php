ff503bb2f8d63ffec7ac711e900900a132c78d43
Ant0C <cafeo97@gmail.com>
2023-05-24 12:28:13 +0200

    FIX header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -20,13 +20,11 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{ url('/') }}">{{ __('Home') }}</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('dishes.index') }}">{{ __('Dishes') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('dishes.create') }}">{{ __('Create') }}</a>
-                        </li>
                         @auth
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('dishes.create') }}">{{ __('Create') }}</a>
+                            </li>
+
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('dishes.index') }}">{{ __('Dishes') }}</a>
                             </li>
