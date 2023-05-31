273f87d7b8e55602bba4fa0e1ea0a4c47a8e3750
francescogentile25 <francescogentile250893@gmail.com>
2023-05-31 17:48:38 +0200

    STYLE to  dishes views and  style to views layout

--- resources/views/restaurants/index.blade.php
+++ resources/views/restaurants/index.blade.php
@@ -1,17 +1,24 @@
 @extends('layouts.app')
 @section('content')
-    <h1>IL TUO RISTORANTE :</h1>
     @forelse($restaurants as $restaurant)
-        <h1>{{ $restaurant->restaurant_name }}</h1>
+        <h1> Ciao {{ Auth::user()->name }} ecco il tuo ristorante:</h1>
+        <div class="card" style="width:25rem">
+            @if ($restaurant->cover_image)
+                <div class="container">
+                    <img src="{{ $restaurant->cover_path }}" alt="">
+                </div>
+            @else
+                <div class="container">
+                    <img src="https://cwdaust.com.au/wpress/wp-content/uploads/2015/04/placeholder-restaurant-300x300.png"
+                        alt="">
+                </div>
+            @endif
 
-        <h2>{{ $restaurant->address }}</h2>
-
-        <p>{{ $restaurant->description }}</p>
-        @if ($restaurant->cover_image)
-            <div class="container">
-                <img src="{{ $restaurant->cover_path }}" alt="">
+            <div class="card-body">
+                <h1>{{ $restaurant->restaurant_name }}</h1>
+                <p class="card-text">{{ $restaurant->description }}</p>
             </div>
-        @endif
+        </div>
     @empty
         -
     @endforelse
