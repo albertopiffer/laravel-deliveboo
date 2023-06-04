499fdcfb08d4792579592ef3bc30d68ba7c74ce7
francescogentile25 <francescogentile250893@gmail.com>
2023-06-04 03:21:21 +0200

    BUONANOTTE

--- resources/views/dishes/create.blade.php
+++ resources/views/dishes/create.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container">
+    <div class="container mt-4">
         <form action="{{ route('dishes.store') }}" method="POST" enctype="multipart/form-data">
 
             @csrf
